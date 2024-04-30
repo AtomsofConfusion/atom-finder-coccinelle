91017a2b6b51c444eaf3a30b122e4343bdeacf97
FNZhao <zhaofn1001@gmail.com>
2024-04-30 14:47:15 -0400

    Omitted Curly Braces with nested if

--- input/omitted_curly_braces.c
+++ input/omitted_curly_braces.c
@@ -4,14 +4,15 @@ int main() {
     int x = 0;
     int y = 5;
 
+// if structure
     if (x < y-1)
-        printf("'if' without braces\n"); //R1
+        printf("'if' without braces\n");
     
     if (x > y)
         printf("if without braces\n");
 
-    if (x < y)  // this is currently not getting detected
-        printf("'if' without braces before an else\n"); //R1
+    if (x < y)
+        printf("'if' without braces before an else\n");
     else 
         printf("'else' without braces after if without braces\n");
 
@@ -27,9 +28,9 @@ int main() {
     }
 
     if (x > y)
-        printf("if without curly braces\n"); //R1
+        printf("if without curly braces\n");
     else if (x < y)
-        printf("else if without curly braces\n"); //R1
+        printf("else if without curly braces\n");
     
     if (x > y) {
         printf("if with curly braces\n");
@@ -37,6 +38,189 @@ int main() {
         printf("else if with curly braces\n");
     }
 
+    // if - else if - else
+    if (x > y) {
+        printf("if with curly braces\n");
+    } else if (x == y) {
+        printf("else if with curly braces\n");
+    } else {
+        printf("else with curly braces\n");
+    }
+
+    if (x > y) 
+        printf("if without curly braces\n");
+    else if (x == y) 
+        printf("else if without curly braces\n");
+    else 
+        printf("else without curly braces\n");
+
+    // if - else if - else if - else
+    if (x > 10) {
+        printf("if with curly braces\n");
+    } else if (x > 5) {
+        printf("else if with curly braces\n"); 
+    } else if (x > 0) {
+        printf("else if with curly braces\n");
+    } else {
+        printf("else with curly braces\n");
+    }
+
+    if (x > 10) 
+        printf("if without curly braces\n");
+    else if (x > 5) 
+        printf("else if without curly braces\n"); 
+    else if (x > 0) 
+        printf("else if without curly braces\n");
+    else 
+        printf("else without curly braces\n");
+
+    if (x > 10) {
+        printf("if with curly braces\n");
+    } else if (x > 9) {
+        printf("else if with curly braces\n");
+    } else if (x > 8) {
+        printf("else if with curly braces\n");
+    } else if (x > 7)
+        printf("else if with curly braces\n");
+    else if (x > 6)
+        printf("else if with curly braces\n");
+    else {
+        printf("else with curly braces\n");
+    }
+
+    // nested if structure
+    if (x > 15) {
+
+        if (x > 19) {
+            printf("nested if with curly braces\n");
+        } else if (x > 18)
+            printf("nested else if without curly braces\n");
+        else if (x > 17)
+            printf("nested else if without curly braces\n");
+        else if (x > 16) {
+            printf("nested else if with curly braces");
+        } else if (x > 15) {
+            printf("nested else if with curly braces");
+        } else{
+            printf("nested else without curly braces\n");
+        }
+
+    } else if (x > 10) {
+
+        if (x > 9) {
+            printf("nested if with curly braces\n");
+        } else if (x > 8)
+            printf("nested else if without curly braces\n");
+        else if (x > 7)
+            printf("nested else if without curly braces\n");
+        else if (x > 6) {
+            printf("nested else if with curly braces");
+        } else if (x > 5) {
+            printf("nested else if with curly braces");
+        } else{
+            printf("nested else without curly braces\n");
+        }
+
+    } else if (x > 5) {
+
+        if (x > 4) {
+            printf("nested if with curly braces\n");
+        } else if (x > 3)
+            printf("nested else if without curly braces\n");
+        else if (x > 2)
+            printf("nested else if without curly braces\n");
+        else if (x > 1) {
+            printf("nested else if with curly braces");
+        } else if (x > 0) {
+            printf("nested else if with curly braces");
+        } else{
+            printf("nested else without curly braces\n");
+        }
+
+
+    } else {
+
+        if (x > -1) {
+            printf("nested if with curly braces\n");
+        } else if (x > -2)
+            printf("nested else if without curly braces\n");
+        else if (x > -3)
+            printf("nested else if without curly braces\n");
+        else if (x > -4) {
+            printf("nested else if with curly braces");
+        } else if (x > -5) {
+            printf("nested else if with curly braces");
+        } else{
+            printf("nested else without curly braces\n");
+        }
+
+    }
+
+    if (x > 15)
+
+        if (x > 19) {
+            printf("nested if with curly braces\n");
+        } else if (x > 18)
+            printf("nested else if without curly braces\n");
+        else if (x > 17)
+            printf("nested else if without curly braces\n");
+        else if (x > 16) {
+            printf("nested else if with curly braces");
+        } else if (x > 15) {
+            printf("nested else if with curly braces");
+        } else {
+            printf("nested else without curly braces\n");
+        }
+
+    else if (x > 10)
+
+        if (x > 9) {
+            printf("nested if with curly braces\n");
+        } else if (x > 8)
+            printf("nested else if without curly braces\n");
+        else if (x > 7)
+            printf("nested else if without curly braces\n");
+        else if (x > 6) {
+            printf("nested else if with curly braces");
+        } else if (x > 5) {
+            printf("nested else if with curly braces");
+        } else {
+            printf("nested else without curly braces\n");
+        }
+
+    else if (x > 5)
+
+        if (x > 4) {
+            printf("nested if with curly braces\n");
+        } else if (x > 3)
+            printf("nested else if without curly braces\n");
+        else if (x > 2)
+            printf("nested else if without curly braces\n");
+        else if (x > 1) {
+            printf("nested else if with curly braces");
+        } else if (x > 0) {
+            printf("nested else if with curly braces");
+        } else{
+            printf("nested else without curly braces\n");
+        }
+        
+    else 
+
+        if (x > -1) {
+            printf("nested if with curly braces\n");
+        } else if (x > -2)
+            printf("nested else if without curly braces\n");
+        else if (x > -3)
+            printf("nested else if without curly braces\n");
+        else if (x > -4) {
+            printf("nested else if with curly braces");
+        } else if (x > -5) {
+            printf("nested else if with curly braces");
+        } else{
+            printf("nested else without curly braces\n");
+        }
+
+// loop
     while (x < 5)
         printf("'while' without braces, x: %d\n", x++);
 
@@ -46,6 +230,14 @@ int main() {
 
     for (int i = 0; i < 5; i++)
         printf("'for' iteration: %d\n", i);
+    
+    do {
+        printf("do while iteration: %d\n", x++);
+    } while (x < 15);
+
+    do
+        printf("do while iteration: %d\n", x++);
+    while (x < 20);
 
     return 0;
 }
