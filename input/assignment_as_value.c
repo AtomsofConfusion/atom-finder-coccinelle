277d92e37057ba6dc54462117754c837260db33e
FNZhao <zhaofn1001@gmail.com>
2024-05-01 17:14:05 -0400

    Handle for loop in Assignment as Value

--- input/assignment_as_value.c
+++ input/assignment_as_value.c
@@ -15,5 +15,18 @@ int main() {
 
     printf("v1: %d, v2: %d\n", v1, v2);
 
+    int v4 = v1 = 6;
+
+    int v5 = v2 = v3 + 1;
+
+    // In specific structure
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", i);
+    }
+
+    for (int i = v5 = 0; i < 5; i++) {
+        printf("%d\n", i);
+    }
+
     return 0;
 }
