8af88e2fd8eddac9ac36a2db5db0e6679e66c157
Austin Huang <im@austinhuang.me>
2024-08-03 16:59:26 -0400

    feat: accept multiple input files

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ def cli():
     parser = argparse.ArgumentParser(
         description="Apply coccinelle patches onto C files"
     )
-    parser.add_argument("file", help="C file to run patches on")
+    parser.add_argument("-f", "--file", default=[], help="""
+        List of C files to run patches on, separated by spaces. If not specified, all *.c files
+        in the current directory will be run.
+        """, nargs='*')
     parser.add_argument("-p", "--patch", default=[], help="""
         List of coccinelle patches to run, separated by spaces. If not specified, all *.cocci files
         in the current directory will be run.
@@ -22,6 +25,14 @@ def cli():
         print("ERROR: coccinelle should be installed before running this tool. Quitting.", file=stderr)
         exit(1)
 
+    # handle files
+    f = args.file
+    if len(f) == 0:
+        print("WARNING: No C files supplied via the -f option. Attempting to find C files here...", file=stderr)
+        f = glob("*.c")
+    if len(f) == 0:
+        print("ERROR: No C files found in the current directory. Quitting.", file=stderr)
+        exit(1)
 
     # handle patches
     patches = args.patch
@@ -30,11 +41,6 @@ def cli():
         patches = glob("*.cocci")
     if len(patches) == 0:
         print("ERROR: No patches found in the current directory. Quitting.", file=stderr)
-        exit(1)
-
-    # handle file
-    if not os.path.isfile(args.file):
-        print(f"ERROR: {args.file} does not exist. Quitting.", file=stderr)
         exit(1)
 
     # handle opts
@@ -48,20 +54,24 @@ def cli():
         args.opts = args.opts[0:clear] + args.opts[clear + 2:]
 
     # run
-    for patch in patches:
-        if not os.path.isfile(patch):
-            print(f"WARNING: patch {patch} does not exist. Skipping.", file=stderr)
+    for c in f:
+        if not os.path.isfile(c):
+            print(f"WARNING: file {c} does not exist. Skipping.", file=stderr)
             continue
-        try:
-            run = subprocess.run(
-                ["spatch", "--sp-file", patch, args.file, "--python", executable] + args.opts,
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, universal_newlines=True
-            )
-            p = run.stdout.strip()
-            if len(p) > 0:
-                print(p)
-        except subprocess.CalledProcessError as e:
-            print(f"STDERR in {patch}: {e.stderr.strip()}", file=stderr)
+        for patch in patches:
+            if not os.path.isfile(patch):
+                print(f"WARNING: patch {patch} does not exist. Skipping.", file=stderr)
+                continue
+            try:
+                run = subprocess.run(
+                    ["spatch", "--sp-file", patch, c, "--python", executable] + args.opts,
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, universal_newlines=True
+                )
+                p = run.stdout.strip()
+                if len(p) > 0:
+                    print(p)
+            except subprocess.CalledProcessError as e:
+                print(f"STDERR in {patch}: {e.stderr.strip()}", file=stderr)
 
 if __name__ == "__main__":
     cli()
