85aea4f469fdd816cf45adcd1d7cdb1fcab985c3
Leyang Wang <131626208+lw3266@users.noreply.github.com>
2024-12-19 13:05:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author="The Atoms of Confusion Project",
     packages=["src"],
     include_package_data=True,
-    install_requires=["pandas", "click", "pytest"],
+    install_requires=["click", "pytest"],
     entry_points={
         "console_scripts": [
             "aoc-cocci = src.tools.aoc_cocci:atom_finder",
